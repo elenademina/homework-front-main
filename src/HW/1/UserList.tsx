9bb028eef24f32797681f48cf6359328cda34f09
Elena <elena.s.demina@gmail.com>
2023-09-15 12:10:06 +0200

    github pages

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,29 +1,33 @@
 type AddressType = {
-  street: any; // ПОДПРАВЛЯЕМ any
-  city: any; // ПОДПРАВЛЯЕМ any
+    street: string;
+    city: string;
 };
 
 type UserType = {
-  // ПРИДЕТСЯ САМОМУ)
+    id: number
+    name: string
+    age: number
+    address: AddressType
 };
 
 type UserListPropsType = {
-  users: UserType; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+    title: string
+    users: Array<UserType>; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
 };
 
-export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
-  return (
-    <div id={'hw01-users'}>
-      <h2>User List:</h2>
+export const UserList = (props: UserListPropsType) => {
+    return (
+        <div id={'hw01-users'}>
+            <h2>{props.title}</h2>
 
-      {/*<ul>*/}
-      {/*  {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...*/}
-      {/*    <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>*/}
-      {/*      <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>*/}
-      {/*      {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
-    </div>
-  );
+            <ul>
+                {props.users.map((objectFromUsersArray, index) => (
+                    <li key={objectFromUsersArray.id} id={`hw01-user-${objectFromUsersArray.id}`}>
+                        <strong>{objectFromUsersArray.name}</strong> (Age: {objectFromUsersArray.age})<strong> Address:</strong>
+                        {objectFromUsersArray.address.street}, {objectFromUsersArray.address.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
